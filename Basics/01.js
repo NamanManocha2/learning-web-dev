9cf4700562ab385ddf5f997f227e293874a3b102
Naman Manocha <namanmanocha02@gmail.com>
2025-08-07 08:38:47 +0000

    Conversion

--- Basics/01.js
+++ Basics/01.js
@@ -1,1 +1,13 @@
 console.log("Hello")
+
+console.log("1" + 2 + 3) // 123
+console.log(1 + 2 + "3") //33
+
+console.log(+true) //1
+console.log(+"") // 0
+
+let gameCount = 100
+gameCount++
+console.log(gameCount);
+
+
