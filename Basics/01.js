997122ba05916dd03397719cd0bc16edd0e1a22f
Naman Manocha <namanmanocha02@gmail.com>
2025-08-07 09:28:31 +0000

    DataType Summary

--- Basics/01.js
+++ Basics/01.js
@@ -1,13 +1,31 @@
-console.log("Hello")
+// console.log("Hello")
 
-console.log("1" + 2 + 3) // 123
-console.log(1 + 2 + "3") //33
+// console.log("1" + 2 + 3) // 123
+// console.log(1 + 2 + "3") //33
 
-console.log(+true) //1
-console.log(+"") // 0
+// console.log(+true) //1
+// console.log(+"") // 0
 
-let gameCount = 100
-gameCount++
-console.log(gameCount);
+// let gameCount = 100
+// gameCount++
+// console.log(gameCount);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // true
+
+// const id = Symbol('123')
+// const otherId = Symbol('123')
+// console.log(id === otherId);
+
+const cars = ["BMW M3", "Porche 911 GT 3 RS", "Mercedes AMG G63"];
+let student = {
+    name: "Naman",
+    age: 21
+}
+console.log(cars);
+console.log(student);
+
+
 
 
