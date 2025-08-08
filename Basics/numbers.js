a3591bc09dfe8fcccd74e18d48089fb92918a1ff
Naman Manocha <namanmanocha02@gmail.com>
2025-08-08 04:01:14 +0000

    Numbers & Maths

---
+++ Basics/numbers.js
@@ -0,0 +1,27 @@
+// let age = 21
+// console.log(age);
+
+// let newAge = new Number(25)
+// console.log(newAge);
+
+// console.log(age.toString());
+
+// let bigNum = 1000000000
+// console.log(bigNum.toLocaleString());
+// console.log(bigNum.toLocaleString('en-IN'));
+
+// Math
+
+// console.log(Math);
+// console.log(Math.abs(-21));
+// console.log(Math.round(4.49));
+// console.log(Math.ceil(4.01));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(23,4343,35,212,1,2,345));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
