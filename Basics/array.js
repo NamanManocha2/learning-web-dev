98f2c140b7a065e965360c971e4e25f0c1775bd2
Naman Manocha <namanmanocha02@gmail.com>
2025-08-09 10:50:04 +0000

    Arrays

---
+++ Basics/array.js
@@ -0,0 +1,40 @@
+let cars = ["BMW M2", "Audi R8"]
+
+// console.log(cars[1]);
+// cars.push("AMG G63")
+// console.log(cars)
+// cars.pop()
+// console.log(cars)
+// cars.unshift("AMF G63")
+// console.log(cars)
+// cars.shift()
+// console.log(cars)
+// cars.includes()
+// cars.indexOf()
+
+// let newCar = cars.join()
+// console.log(typeof newCar);
+
+// slice doesn't manipulate the original array but splice does
+
+let bikes = ["S100RR", "SuperDuke 1290", "Hypermotard 698"]
+
+// let vehicles = cars.concat(bikes)
+// console.log(vehicles);
+
+// let engine = [...cars, ...bikes]
+// console.log(engine);
+
+// let arr = [1,2,3,[4,5,6,[7,8,9]]]
+// let usableArr = arr.flat(3)
+// console.log(usableArr);
+
+// console.log(Array.isArray("Naman"));
+// console.log(Array.from("Naman"));
+// console.log(Array.from({name: "Naman"}));
+
+let age1=21
+let age2=25
+let age3=27
+
+console.log(Array.of(age1,age2,age3));
